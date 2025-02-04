bd9096deb399a841dc9b17979d1a031af67b25ac
Akashmed <akashmed3@gmail.com>
2025-02-04 23:08:42 +0600

    Optimized middleware for production (token key)

--- src/middleware.js
+++ src/middleware.js
@@ -1,18 +1,17 @@
-import { cookies } from "next/headers"
-import { NextResponse } from "next/server"
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
+export const middleware = async (request) => {
+    // In production, the token is stored as "__Secure-next-auth.session-token"
+    const token = await cookies(request).get('next-auth.session-token') || await cookies(request).get('__Secure-next-auth.session-token');
 
-export const middleware = async (request) =>{
-    const token = await cookies(request).get('next-auth.session-token')
-    // console.log(token, 'token')
-
-    if(!token){
+    if (!token) {
         return NextResponse.redirect(new URL('/login', request.url));
     }
 
     return NextResponse.next();
-}
+};
 
 export const config = {
     matcher: ['/checkout', '/admin', '/myBookings']
-}
+};
